95f2904b77252ba1ede7f3de310930524d7c3ebf
Joshua Gould <jgould@broadinstitute.org>
2017-11-09 14:11:59 -0500

    changed " to '

--- bin/ot.py
+++ bin/ot.py
@@ -8,74 +8,83 @@ import numpy as np
 import sklearn.metrics.pairwise
 
 parser = argparse.ArgumentParser(
-    description="Compute transport maps between successive time points")
+    description='Compute transport maps between successive time points')
 
-parser.add_argument("--expression_file",
-                    help="Gene expression file with cells on rows and "
-                         "features on columns",
+parser.add_argument('--expression_file',
+                    help='Gene expression file with cells on rows and '
+                         'features on columns',
                     required=True)
-parser.add_argument("--growth_file",
-                    help="Two column file without header with cell ids and "
-                         "growth scores",
+parser.add_argument('--growth_file',
+                    help='Two column file without header with cell ids and '
+                         'relative growth scores. If not specified, uniform '
+                         'growth is assumed.',
+                    required=False)
+parser.add_argument('--days_file',
+                    help='Two column file without header with cell ids and '
+                         'days',
                     required=True)
-parser.add_argument("--days_file",
-                    help="Two column file without header with cell ids and "
-                         "days",
+parser.add_argument('--epsilon', type=float,
+                    help='Controls the entropy of the transport map. An '
+                         'extremely large entropy parameter will give a '
+                         'maximally entropic transport map, and an '
+                         'extremely '
+                         'small entropy parameter will give a nearly '
+                         'deterministic transport map (but could also '
+                         'lead to '
+                         'numerical instability in the algorithm')
+parser.add_argument('--prefix',
+                    help='Prefix for ouput file names',
                     required=True)
-parser.add_argument("--epsilon", type=float,
-                    help="Controls the entropy of the transport map. An "
-                         "extremely large entropy parameter will give a "
-                         "maximally entropic transport map, and an "
-                         "extremely "
-                         "small entropy parameter will give a nearly "
-                         "deterministic transport map (but could also "
-                         "lead to "
-                         "numerical instability in the algorithm")
-parser.add_argument("--prefix",
-                    help="Prefix for ouput file names",
-                    required=True)
-parser.add_argument("--max_transport_fraction",
+
+parser.add_argument('--max_transport_fraction',
                     default=0.4,
-                    help="The maximum fraction of cells at time t that are "
-                         "transported to time t + 1",
+                    help='The maximum fraction of cells at time t that are '
+                         'transported to time t + 1',
                     type=float)
-parser.add_argument("--min_transport_fraction",
+parser.add_argument('--min_transport_fraction',
                     default=0.05,
-                    help="The minimum fraction of cells at time t that are "
-                         "transported to time t + 1",
+                    help='The minimum fraction of cells at time t that are '
+                         'transported to time t + 1',
                     type=float)
-parser.add_argument("--lambda1", default=1,
-                    help="Regularization parameter that controls the "
-                         "fidelity "
-                         "of the constraints on p",
+parser.add_argument('--lambda1', default=1,
+                    help='Regularization parameter that controls the '
+                         'fidelity '
+                         'of the constraints on p',
                     type=float)
-parser.add_argument("--lambda2", default=1,
-                    help="Regularization parameter that controls the "
-                         "fidelity "
-                         "of the constraints on q",
+parser.add_argument('--lambda2', default=1,
+                    help='Regularization parameter that controls the '
+                         'fidelity '
+                         'of the constraints on q',
                     type=float)
-parser.add_argument("--growth_ratio", type=float, default=2.5,
-                    help="Over 1 day, a cell in the more proliferative "
-                         "group "
-                         "is expected to produce growth_ratio times as "
-                         "many "
-                         "offspring as a cell in the non-proliferative "
-                         "group")
-parser.add_argument("--scaling_iter", default=250,
-                    help="Number of scaling iterations", type=int)
-parser.add_argument("--min_growth_fit", type=float, default=0.9)
-parser.add_argument("--l0_max", type=float, default=100)
-parser.add_argument("--verbose", action="store_true",
-                    help="Print progress information")
-
+parser.add_argument('--growth_ratio', type=float, default=2.5,
+                    help='Over 1 day, a cell in the more proliferative '
+                         'group '
+                         'is expected to produce growth_ratio times as '
+                         'many '
+                         'offspring as a cell in the non-proliferative '
+                         'group')
+parser.add_argument('--scaling_iter', default=250,
+                    help='Number of scaling iterations', type=int)
+parser.add_argument('--min_growth_fit', type=float, default=0.9)
+parser.add_argument('--l0_max', type=float, default=100)
+parser.add_argument('--compress', action='store_true',
+                    help='Compress output files')
+parser.add_argument('--verbose', action='store_true',
+                    help='Print progress information')
 args = parser.parse_args()
 
 # cells on rows, features on columns
 gene_expression = pandas.read_table(args.expression_file, index_col=0)
-growth_scores = pandas.read_table(args.growth_file, index_col=0, header=None,
-                                names=["growth_score"])
+
 days = pandas.read_table(args.days_file, index_col=0, header=None,
-                       names=["day"])
+                         names=['day'])
+if args.growth_file is not None:
+    growth_scores = pandas.read_table(args.growth_file, index_col=0,
+                                      header=None,
+                                      names=['growth_score'])
+else:
+    growth_scores = pandas.DataFrame(index=days.index, columns=['growth_score'],
+                                     data=1)
 
 gene_expression = gene_expression.join(growth_scores).join(days)
 
@@ -87,25 +96,26 @@ group_by_day = gene_expression.groupby(day_field_name)
 days = list(group_by_day.groups.keys())
 days.sort()
 if len(days) == 1:
-    print("Only one unique day found.")
+    print('Only one unique day found.')
     exit(1)
 
 if args.verbose:
-    print(str(len(days)) + " unique days")
+    print(str(len(days)) + ' unique days')
+
 for i in range(len(days) - 1):
     # consecutive days only
     m1 = group_by_day.get_group(days[i])
     m2 = group_by_day.get_group(days[i + 1])
     if args.verbose:
         print(
-            "Computing transport map from day " + str(
-                days[i]) + " to day " + str(
+            'Computing transport map from day ' + str(
+                days[i]) + ' to day ' + str(
                 days[i + 1]))
     delta_t = days[i + 1] - days[i]
     cost_matrix = sklearn.metrics.pairwise.pairwise_distances(
         m1.drop(fields_to_drop_for_distance, axis=1),
         Y=m2.drop(fields_to_drop_for_distance, axis=1),
-        metric="sqeuclidean")
+        metric='sqeuclidean')
     cost_matrix = cost_matrix / np.median(cost_matrix)
     growth_rate = m1.growth_score.values
     result = wot.optimal_transport(cost_matrix, growth_rate,
@@ -118,9 +128,11 @@ for i in range(len(days) - 1):
                                    epsilon=args.epsilon,
                                    growth_ratio=args.growth_ratio,
                                    scaling_iter=args.scaling_iter)
-    transport = pandas.DataFrame(result["transport"], index=m1.index,
+    transport = pandas.DataFrame(result['transport'], index=m1.index,
                                  columns=m2.index)
+
     if args.verbose:
-        print("Saving transport map")
-    transport.to_csv(args.prefix + "_" + str(days[i]) + "_" + str(
-        days[i + 1]) + ".txt", index_label="id", sep="\t")
+        print('Saving transport map')
+    transport.to_csv(args.prefix + '_' + str(days[i]) + '_' + str(
+        days[i + 1]) + '.txt', index_label='id', sep='\t', compression='gzip' if
+    args.compress else None)
--- bin/trajectory.py
+++ bin/trajectory.py
@@ -7,24 +7,23 @@ import os.path
 import wot
 
 parser = argparse.ArgumentParser(
-    description="Compute ancestors and descendants given cell ids, a time t, "
-                "and transport maps")
-
-parser.add_argument("--dir",
-                    help="Directory of transport maps as produced by ot",
+    description='Compute ancestors and descendants given cell ids, a time t, '
+                'and transport maps')
+parser.add_argument('--dir',
+                    help='Directory of transport maps as produced by ot',
                     required=True)
-parser.add_argument("--id",
-                    help="A list of cell ids at time t to compute the "
-                         "trajectory "
-                         "for. Can either be provided in a file or as command "
-                         "line arguments",
-                    required=True, action="append")
-parser.add_argument("--time",
-                    help="The time t",
+parser.add_argument('--id',
+                    help='A list of cell ids at time t to compute the '
+                         'trajectory '
+                         'for. Can either be provided in a file or as command '
+                         'line arguments',
+                    required=True, action='append')
+parser.add_argument('--time',
+                    help='The time t',
                     required=True)
-parser.add_argument("--prefix",
-                    help="Prefix for ouput file names. Command produces "
-                         "prefix_ancestors.csv and prefix_descendants.csv",
+parser.add_argument('--prefix',
+                    help='Prefix for ouput file names. Command produces '
+                         'prefix_ancestors.csv and prefix_descendants.csv',
                     required=True)
 
 args = parser.parse_args()
@@ -42,9 +41,10 @@ else:
 transport_maps_inputs = []  # file, start, end
 for f in os.listdir(input_dir):
     if os.path.isfile(os.path.join(input_dir, f)):
-        split = os.path.splitext(f)
-        if split[1] == ".txt":
-            tokens = split[0].split('_')
+        file_info = wot.get_file_basename_and_extension(f)
+        if file_info['ext'] == '.txt':
+            basename = file_info['basename']
+            tokens = basename.split('_')
             path = os.path.join(input_dir, f)
             t = tokens[len(tokens) - 1]
             t_minus_1 = tokens[len(tokens) - 2]
@@ -54,14 +54,14 @@ for f in os.listdir(input_dir):
             if t == time:
                 transport_map = transport_map[ids]
             transport_maps_inputs.append(
-                {"transport_map": transport_map,
-                 "t_minus_1": t_minus_1, "t": t, "t_minus_1_f": float(
+                {'transport_map': transport_map,
+                 't_minus_1': t_minus_1, 't': t, 't_minus_1_f': float(
                     t_minus_1)})
 transport_maps_inputs.sort(
-    key=lambda x: x["t_minus_1_f"])  # sort by t_minus_1 (start time)
+    key=lambda x: x['t_minus_1_f'])  # sort by t_minus_1 (start time)
 
 result = wot.trajectory(ids, transport_maps_inputs, time)
-result["ancestors"].to_csv(prefix + "_ancestors.txt", index_label="id",
-                           sep="\t")
-result["descendants"].to_csv(prefix + "_descendants.txt", index_label="id",
-                             sep="\t")
+result['ancestors'].to_csv(prefix + '_ancestors.txt', index_label='id',
+                           sep='\t')
+result['descendants'].to_csv(prefix + '_descendants.txt', index_label='id',
+                             sep='\t')
