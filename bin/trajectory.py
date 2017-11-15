85daa411e47a72148cb920cf36b1c5d2bad84443
Joshua Gould <jgould@broadinstitute.org>
2017-11-15 14:51:37 -0500

    read ids from file

--- bin/trajectory.py
+++ bin/trajectory.py
@@ -12,18 +12,16 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--dir',
                     help='Directory of transport maps as produced by ot',
                     required=True)
-parser.add_argument('--id',
-                    help='A list of cell ids at time t to compute the '
-                         'trajectory '
-                         'for. Can either be provided in a file or as command '
-                         'line arguments',
-                    required=True, action='append')
 parser.add_argument('--time',
                     help='The time t',
                     required=True)
 parser.add_argument('--prefix',
                     help='Prefix for ouput file names. Command produces '
-                         'prefix_ancestors.csv and prefix_descendants.csv',
+                         'prefix_ancestors.txt and prefix_descendants.txt',
+                    required=True)
+parser.add_argument('--id',
+                    help='A list of cell ids at time t to compute the '
+                         'trajectory for.',
                     required=True)
 parser.add_argument('--compress', action='store_true',
                     help='gzip output files')
@@ -32,11 +30,7 @@ args = parser.parse_args()
 input_dir = args.dir
 time = args.time
 prefix = args.prefix
-
-if os.path.isfile(args.id[0]):
-    ids = pandas.read_table(args.id)
-else:
-    ids = args.id
+ids = pandas.read_table(args.id)[:, 0]
 
 # transport map file names end with start_end.csv
 # csv has prior time on rows, later time on columns
@@ -44,21 +38,20 @@ transport_maps_inputs = []  # file, start, end
 for f in os.listdir(input_dir):
     if os.path.isfile(os.path.join(input_dir, f)):
         file_info = wot.get_file_basename_and_extension(f)
-        if file_info['ext'] == '.txt':
-            basename = file_info['basename']
-            tokens = basename.split('_')
-            path = os.path.join(input_dir, f)
-            t = tokens[len(tokens) - 1]
-            t_minus_1 = tokens[len(tokens) - 2]
-            transport_map = pandas.read_table(path, index_col=0)
-            if t_minus_1 == time:
-                transport_map = transport_map[transport_map.index.isin(ids)]
-            if t == time:
-                transport_map = transport_map[ids]
-            transport_maps_inputs.append(
-                {'transport_map': transport_map,
-                 't_minus_1': t_minus_1, 't': t, 't_minus_1_f': float(
-                    t_minus_1)})
+        basename = file_info['basename']
+        tokens = basename.split('_')
+        path = os.path.join(input_dir, f)
+        t = tokens[len(tokens) - 1]
+        t_minus_1 = tokens[len(tokens) - 2]
+        transport_map = pandas.read_table(path, index_col=0)
+        if t_minus_1 == time:
+            transport_map = transport_map[transport_map.index.isin(ids)]
+        if t == time:
+            transport_map = transport_map[ids]
+        transport_maps_inputs.append(
+            {'transport_map': transport_map,
+             't_minus_1': t_minus_1, 't': t, 't_minus_1_f': float(
+                t_minus_1)})
 transport_maps_inputs.sort(
     key=lambda x: x['t_minus_1_f'])  # sort by t_minus_1 (start time)
 
