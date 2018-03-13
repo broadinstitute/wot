7f64cdf03a2ce128a5de5cc63d0c20dfa63f796c
jgould <jgould@broadinstitute.org>
2018-03-13 13:05:44 -0400

    skip hidden files

--- bin/summarize_by_cluster.py
+++ bin/summarize_by_cluster.py
@@ -35,19 +35,19 @@ args = parser.parse_args()
 input_dir = args.dir
 cluster_transport_maps = []
 clusters = pd.read_table(args.clusters, index_col=0, header=None,
-                             names=['cluster'], quoting=csv.QUOTE_NONE)
+                         names=['cluster'], quoting=csv.QUOTE_NONE)
 grouped_by_cluster = clusters.groupby(clusters.columns[0], axis=0)
 cluster_ids = list(grouped_by_cluster.groups.keys())
 column_cell_ids_by_time = []
 all_cell_ids = set()
 
 for f in os.listdir(input_dir):
-    if os.path.isfile(os.path.join(input_dir, f)):
+    if os.path.isfile(os.path.join(input_dir, f)) and not f.startswith('.'):
         file_info = wot.io.get_file_basename_and_extension(f)
         basename = file_info[0]
         path = os.path.join(input_dir, f)
         transport_map = pd.read_table(path, index_col=0,
-                                          quoting=csv.QUOTE_NONE)
+                                      quoting=csv.QUOTE_NONE)
         all_cell_ids.update(transport_map.columns)
         all_cell_ids.update(transport_map.index)
         column_cell_ids_by_time.append(transport_map.columns)
@@ -65,10 +65,10 @@ for f in os.listdir(input_dir):
         cluster_transport_maps.append(cluster_transport_map)
 
 weights = wot.ot.get_weights(all_cell_ids, column_cell_ids_by_time,
-                          grouped_by_cluster, cluster_ids)
+                             grouped_by_cluster, cluster_ids)
 cluster_weights_by_time = weights['cluster_weights_by_time']
 combined_cluster_map = wot.ot.transport_maps_by_time(cluster_transport_maps,
-                                                  cluster_weights_by_time)
+                                                     cluster_weights_by_time)
 combined_cluster_map.to_csv(args.prefix + '.txt' + ('.gz' if
                                                     args.compress else ''),
                             index_label="id",
