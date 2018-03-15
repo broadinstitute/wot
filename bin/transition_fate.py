458410778972c762db4a29d2b2ac5325980694b2
jgould <jgould@broadinstitute.org>
2018-03-15 16:26:59 -0400

    specify multiple start times

--- bin/transition_fate.py
+++ bin/transition_fate.py
@@ -9,13 +9,13 @@ import csv
 import pandas as pd
 
 parser = argparse.ArgumentParser(
-    description='Compute the trajectory')
+    description='Compute cell trajectories')
 parser.add_argument('--dir',
                     help='Directory of transport maps as produced by ot',
                     required=True)
 parser.add_argument('--start_time',
                     help='The start time',
-                    required=True, type=float)
+                    required=True, type=float, action='append')
 parser.add_argument('--end_time',
                     help='The end time',
                     required=True, type=float)
@@ -34,54 +34,55 @@ parser.add_argument('--cell_sets',
                     required=True)
 
 args = parser.parse_args()
-start_time = args.start_time
+start_times = args.start_time
 end_time = args.end_time
 prefix = args.prefix
 clusters = pd.read_table(args.clusters, index_col=0, header=None, names=['cluster'], engine='python', sep=None)
 cell_sets = wot.io.read_gene_sets(args.cell_sets)
 transport_maps = wot.io.list_transport_maps(args.dir)
-start_time_index = None
-end_time_index = None
-for i in range(len(transport_maps)):
-    if transport_maps[i]['t1'] == start_time:
-        start_time_index = i
-    if transport_maps[i]['t2'] == end_time:
-        end_time_index = i
 
-if start_time_index is None:
-    raise RuntimeError(
-        'Transport transport_map for time ' + str(start_time) + ' not found.')
-if end_time_index is None:
-    raise RuntimeError(
-        'Transport transport_map for time ' + str(end_time) + ' not found.')
-tmap = None
-for i in range(start_time_index, end_time_index + 1):
-    print(transport_maps[i]['path'])
-    ds = wot.io.read_dataset(transport_maps[i]['path'])
-    tmap_i = pd.DataFrame(index=ds.row_meta.index, columns=ds.col_meta.index, data=ds.x)
-    if tmap is None:
-        tmap = tmap_i
-    else:
-        tmap = tmap.dot(tmap_i / np.sqrt(np.sum(tmap_i.values)))
-nsets = cell_sets.x.shape[1]
-cell_set_ids = cell_sets.col_meta.index.values
-cell_set_id_to_row_indices = {}
-cell_set_id_to_column_indices = {}
-for i in range(len(cell_set_ids)):
-    cell_set_id = cell_set_ids[i]
-    cluster_ids = cell_sets.row_meta.index[cell_sets.x[:, i] > 0]
-    cell_ids = clusters.index.values[clusters['cluster'].isin(cluster_ids)]
-    cell_set_id_to_row_indices[cell_set_id] = np.where(tmap.index.isin(cell_ids))
-    cell_set_id_to_column_indices[cell_set_id] = np.where(np.isin(tmap.columns, cell_ids))
-summary = pd.DataFrame(index=cell_set_ids, columns=cell_set_ids, data=0.0)
-for i in range(nsets):
-    row_indices = cell_set_id_to_row_indices[cell_set_ids[i]]
-    tmap_r = tmap.values[row_indices]
-    for j in range(nsets):
-        column_indices = cell_set_id_to_row_indices[cell_set_ids[j]]
-        summary.iloc[i, j] += tmap_r[:, column_indices].sum()
+for start_time in start_times:
+    start_time_index = None
+    end_time_index = None
+    for i in range(len(transport_maps)):
+        if transport_maps[i]['t1'] == start_time:
+            start_time_index = i
+        if transport_maps[i]['t2'] == end_time:
+            end_time_index = i
 
-# tmap.to_csv(prefix + '_' + str(start_time) + '_' + str(end_time) + '_transition.txt', index_label='id', sep='\t',
-#             doublequote=False, quoting=csv.QUOTE_NONE)
-summary.to_csv(prefix + '_' + str(start_time) + '_' + str(end_time) + '_transition_summary.txt', index_label='id',
-               sep='\t', doublequote=False, quoting=csv.QUOTE_NONE)
+    if start_time_index is None:
+        raise RuntimeError(
+            'Transport transport_map for time ' + str(start_time) + ' not found.')
+    if end_time_index is None:
+        raise RuntimeError(
+            'Transport transport_map for time ' + str(end_time) + ' not found.')
+    tmap = None
+    for i in range(start_time_index, end_time_index + 1):
+        ds = wot.io.read_dataset(transport_maps[i]['path'])
+        tmap_i = pd.DataFrame(index=ds.row_meta.index, columns=ds.col_meta.index, data=ds.x)
+        if tmap is None:
+            tmap = tmap_i
+        else:
+            tmap = tmap.dot(tmap_i / np.sqrt(np.sum(tmap_i.values)))
+    nsets = cell_sets.x.shape[1]
+    cell_set_ids = cell_sets.col_meta.index.values
+    cell_set_id_to_row_indices = {}
+    cell_set_id_to_column_indices = {}
+    for i in range(len(cell_set_ids)):
+        cell_set_id = cell_set_ids[i]
+        cluster_ids = cell_sets.row_meta.index[cell_sets.x[:, i] > 0]
+        cell_ids = clusters.index.values[clusters['cluster'].isin(cluster_ids)]
+        cell_set_id_to_row_indices[cell_set_id] = np.where(tmap.index.isin(cell_ids))
+        cell_set_id_to_column_indices[cell_set_id] = np.where(np.isin(tmap.columns, cell_ids))
+    summary = pd.DataFrame(index=cell_set_ids, columns=cell_set_ids, data=0.0)
+    for i in range(nsets):
+        row_indices = cell_set_id_to_row_indices[cell_set_ids[i]]
+        tmap_r = tmap.values[row_indices]
+        for j in range(nsets):
+            column_indices = cell_set_id_to_row_indices[cell_set_ids[j]]
+            summary.iloc[i, j] += tmap_r[:, column_indices].sum()
+
+    # tmap.to_csv(prefix + '_' + str(start_time) + '_' + str(end_time) + '_transition.txt', index_label='id', sep='\t',
+    #             doublequote=False, quoting=csv.QUOTE_NONE)
+    summary.to_csv(prefix + '_' + str(start_time) + '_' + str(end_time) + '_transition_summary.txt', index_label='id',
+                   sep='\t', doublequote=False, quoting=csv.QUOTE_NONE)
