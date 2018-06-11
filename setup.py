efd3d62be7ded4e7eaeaea5610501a103466b770
jgould <jgould@broadinstitute.org>
2018-06-11 14:57:09 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 requirements = [
-    'h5py >= 2.7.0', 'numpy >=1.8.2', 'SciPy >= 0.13.3',
-    'scikit-learn >=0.19', 'pandas >= 0.23.0', 'POT >= 0.4.0', 'Cython >= 0.28.1', 'Flask >= 1.0.2',
-    'gunicorn >= 19.8.1', 'numexpr >= 2.6.5'
+    'h5py', 'numpy', 'scipy',
+    'scikit-learn', 'pandas>=0.18', 'POT>=0.4', 'Cython', 'Flask',
+    'gunicorn', 'numexpr'
 ]
 
 extras_require = {
-    'GRN': ["'gslrandom>=0.1.4'"],
+    'GRN': ["'gslrandom>=0.1'"],
 }
 
 setup_requirements = [
@@ -42,12 +42,18 @@ setuptools.setup(
     zip_safe=False,
     keywords='wot',
     classifiers=[
+        'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
         'Natural Language :: English',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization',
     ],
     test_suite='tests',
     tests_require=test_requirements,
