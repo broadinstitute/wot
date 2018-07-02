5c814441b5bb62ea5307af0787762ff07d4720ea
David ROBIN <david.robin97@gmail.com>
2018-07-02 19:08:44 -0400

    Add dataset merge and default initializer

--- wot/dataset_util.py
+++ wot/dataset_util.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import wot
+import math
+import numpy
+import pandas
 import scipy.sparse
-from numpy import where
 
 
 # Compute the z-score for each gene in the set. Truncate these z-scores at 5
@@ -51,7 +53,7 @@ def cell_indices_by_day(dataset):
     unique_days = list_of_days_in_dataset(dataset)
     for day in unique_days:
         day_query = dataset.row_meta['day'] == day
-        indices = where(day_query)[0]
+        indices = numpy.where(day_query)[0]
         day_to_indices[day] = indices
     return day_to_indices
 
@@ -66,3 +68,31 @@ def set_cell_metadata(dataset, name, data, indices=None):
         dataset.row_meta[name] = data
     else:
         dataset.row_meta.loc[dataset.row_meta.index[indices], name] = data
+
+def merge_datasets(*args):
+    datasets = list(args)
+    merged_x = numpy.concatenate([ d.x for d in datasets ])
+    row_columns = set(datasets[0].row_meta.columns)
+    if not all([ set(d.row_meta.columns) == row_columns for d in datasets ]):
+        raise ValueError("Unable to merge: incompatible metadata between datasets")
+    merged_row_meta = pandas.concat([ d.row_meta for d in datasets ], sort=True)
+    if merged_row_meta.index.duplicated().any():
+        raise ValueError("Unable to merge: duplicate rows between datasets, cannot lose information")
+    col_index = datasets[0].col_meta.index
+    if not all([ d.col_meta.index.equals(col_index) for d in datasets ]):
+        raise ValueError("Unable to merge: incompatible genes between datasets")
+    merged_col_meta = datasets[0].col_meta
+    return wot.Dataset(merged_x, merged_row_meta, merged_col_meta)
+
+def dataset_from_x(x, rows=None, columns=None,
+        row_prefix="cell_", column_prefix="gene_"):
+    if rows is None:
+        row_count_len = math.floor(math.log10(x.shape[0])) + 1
+        rows = [ "{}{:0{}}".format(row_prefix, i, row_count_len) for i in range(x.shape[0]) ]
+    if columns is None:
+        col_count_len = math.floor(math.log10(x.shape[1])) + 1
+        columns = [ "{}{:0{}}".format(column_prefix, i, col_count_len) for i in range(x.shape[1]) ]
+    return wot.Dataset(x,
+            pandas.DataFrame([], index=rows, columns=[]),
+            pandas.DataFrame([], index=columns, columns=[])
+            )
