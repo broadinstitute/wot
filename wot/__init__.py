9006e01e5c18537a8e41e74fd48b1a7e68729a26
David ROBIN <david.robin97@gmail.com>
2018-07-23 08:53:23 -0400

    Move matplotlib import to __init__ to avoid pyplot use override

--- wot/__init__.py
+++ wot/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import matplotlib
+matplotlib.use('Agg')
+
 from .core import initialize_core
 from .dataset import *
 from .dataset_util import *
--- wot/__main__.py
+++ wot/__main__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import matplotlib
-
 from wot.commands import *
 import argparse
 
