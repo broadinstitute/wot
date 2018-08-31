b385133d4e9f193adb4f6dc97a6ab9fa5037b0cf
jgould <jgould@broadinstitute.org>
2018-08-31 11:30:13 -0400

    version bump

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setuptools.setup(
     name='wot',
-    version='0.2.0',
+    version='0.2.1',
     description="Optimal transport for time-course single cell data",
     author="WOT Team",
     author_email='wot@broadinstitute.org',
