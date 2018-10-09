3427676e786dd99ab9589c139bde8227159a7374
jgould <jgould@broadinstitute.org>
2018-10-09 11:41:37 -0400

    version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setuptools.setup(
     name='wot',
-    version='0.2.2',
+    version='0.2.3',
     description="Optimal transport for time-course single cell data",
     author="WOT Team",
     author_email='wot@broadinstitute.org',
