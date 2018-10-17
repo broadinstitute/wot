c228d99601d4326067b9187d49f83ec88cf23652
jgould <jgould@broadinstitute.org>
2018-10-17 11:47:30 -0400

    fix ot model package

--- wot/commands/optimal_transport.py
+++ wot/commands/optimal_transport.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import argparse
+
+import wot.commands
 import wot.io
 import wot.ot
-import wot.commands
 
 
 def main(argv):
@@ -17,27 +17,27 @@ def main(argv):
     # parser.add_argument('--format', default='loom', help='Transport map file format.',
     #                     choices=wot.commands.FORMAT_CHOICES)
     args = parser.parse_args(argv)
-    ot_model = wot.initialize_ot_model(args.matrix, args.cell_days,
-                                       tmap_out=args.out,
-                                       local_pca=args.local_pca,
-                                       growth_iters=args.growth_iters,
-                                       epsilon=args.epsilon,
-                                       lambda1=args.lambda1,
-                                       lambda2=args.lambda2,
-                                       max_threads=args.max_threads,
-                                       epsilon0=args.epsilon0,
-                                       tau=args.tau,
-                                       day_pairs=args.config,
-                                       cell_day_filter=args.cell_day_filter,
-                                       cell_growth_rates=args.cell_growth_rates,
-                                       gene_filter=args.gene_filter,
-                                       cell_filter=args.cell_filter,
-                                       # output_file_format=args.format,
-                                       sampling_bias=args.sampling_bias,
-                                       scaling_iter=args.scaling_iter,
-                                       inner_iter_max=args.inner_iter_max,
-                                       force=args.force,
-                                       ncells=args.ncells,
-                                       ncounts=args.ncounts
-                                       )
+    ot_model = wot.ot.initialize_ot_model(args.matrix, args.cell_days,
+                                          tmap_out=args.out,
+                                          local_pca=args.local_pca,
+                                          growth_iters=args.growth_iters,
+                                          epsilon=args.epsilon,
+                                          lambda1=args.lambda1,
+                                          lambda2=args.lambda2,
+                                          max_threads=args.max_threads,
+                                          epsilon0=args.epsilon0,
+                                          tau=args.tau,
+                                          day_pairs=args.config,
+                                          cell_day_filter=args.cell_day_filter,
+                                          cell_growth_rates=args.cell_growth_rates,
+                                          gene_filter=args.gene_filter,
+                                          cell_filter=args.cell_filter,
+                                          # output_file_format=args.format,
+                                          sampling_bias=args.sampling_bias,
+                                          scaling_iter=args.scaling_iter,
+                                          inner_iter_max=args.inner_iter_max,
+                                          force=args.force,
+                                          ncells=args.ncells,
+                                          ncounts=args.ncounts
+                                          )
     ot_model.compute_all_transport_maps()
