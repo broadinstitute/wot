00167a670c4fd5a5d2f42e550cddb06d501214e3
jgould <jgould@broadinstitute.org>
2019-04-16 17:14:37 -0400

    Take output from gene_set_scores command

--- wot/commands/cells_by_gene_set.py
+++ wot/commands/cells_by_gene_set.py
@@ -2,30 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-
+import numpy as np
+import sys
 import wot.io
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description= \
-                                         'Compute the list of cells in each gene set')
-    parser.add_argument('--matrix',
-                        help=wot.commands.MATRIX_HELP, required=True)
-    parser.add_argument('--gene_sets',
-                        help='Gene sets in gmx of gmt format', required=True)
-    parser.add_argument('--out', help='Output file name prefix')
-    parser.add_argument('--format', help=wot.commands.FORMAT_HELP,
-                        default='gmt', choices=['gmt', 'gmx', 'txt'])
-    parser.add_argument('--quantile', default=.99,
-                        help='Proportion of cells considered to have high expression')
+    parser = argparse.ArgumentParser(description='Generate cell sets from gene set scores')
+    parser.add_argument('--score',
+                        help='Gene sets scores generated from the gene_set_scores command', required=True,
+                        action='append')
+    parser.add_argument('--out', help='Output file name prefix', required=True)
+    parser.add_argument('--quantile', default=99,
+                        help='Quantile for cells to be considered a member of a cell set', type=float)
 
     args = parser.parse_args(argv)
+    quantile = args.quantile
+    cell_set_name_to_ids = {}
+    for path in args.score:
+        ds = wot.io.read_dataset(path)
+        percentiles = np.percentile(ds.X, axis=0, q=quantile)
+        if len(percentiles.shape) == 0:
+            percentiles = [percentiles]
 
-    if args.out is None:
-        args.out = 'stdout'
+        for j in range(ds.shape[1]):  # each gene set
+            x = ds[:, j].X
+            selected = x >= percentiles[j]
+            cell_ids = ds[selected].obs.index
+            if len(cell_ids) > 0:
+                cell_set_name_to_ids[ds.var.index[j]] = cell_ids
 
-    dataset = wot.io.read_dataset(args.matrix)
-    gene_sets = wot.io.read_sets(args.gene_sets, dataset.var.index.values)
+    if len(cell_set_name_to_ids) == 0:
+        sys.exit('No cells found')
 
-    result = wot.get_cells_in_gene_sets(gene_sets, dataset, quantile=float(args.quantile))
-    wot.io.write_gene_sets(result, args.out, args.format)
+    wot.io.write_gene_sets(cell_set_name_to_ids, args.out)
