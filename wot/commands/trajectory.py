c7c72a4439edda21bab1521c21ec3eeceabbb9b1
jgould <jgould@broadinstitute.org>
2019-04-24 15:54:58 -0400

    removed divergence computation

--- wot/commands/trajectory.py
+++ wot/commands/trajectory.py
@@ -5,29 +5,36 @@ import argparse
 
 import numpy as np
 import pandas as pd
+
 import wot.io
-from matplotlib import pyplot
 
 
 def main(argv):
     parser = argparse.ArgumentParser(
-        'Generate ancestors and descendants of cell sets generated at the given time and computes divergence between trajectories across time')
+        'Generate ancestors and descendants of cell sets generated at the given time.')
     parser.add_argument('--tmap', help=wot.commands.TMAP_HELP, required=True)
     parser.add_argument('--cell_set', help=wot.commands.CELL_SET_HELP, required=True)
     parser.add_argument('--day', help='Day to consider', required=True, type=float)
-    parser.add_argument('--out', help='Prefix for output file names', default='trajectory')
+    parser.add_argument('--out', help='Prefix for output file names', default='wot-trajectory')
     parser.add_argument('--format', help='Output trajectory matrix file format', default='txt')
     parser.add_argument('--embedding', help='Optional file with id, x, y used to plot trajectory probabilities')
-    parser.add_argument('--plot_divergence', help='Whether to plot divergence over time', action='store_true')
+
     args = parser.parse_args(argv)
     tmap_model = wot.tmap.TransportMapModel.from_directory(args.tmap)
+
     cell_sets = wot.io.read_sets(args.cell_set, as_dict=True)
     populations = tmap_model.population_from_cell_sets(cell_sets, at_time=args.day)
-    trajectory_ds = tmap_model.compute_trajectories(populations)
 
-    # dataset has cells on rows and cell sets on columns
+    # print cell size sizes
+    for name in populations:
+        p = populations[name].p
+        print('{}, {}/{}'.format(name, np.count_nonzero(p), len(p)))
+
+    trajectory_ds = tmap_model.compute_trajectories(populations)
+    # dataset has cells on rows and cell sets (trajectories) on columns
     wot.io.write_dataset(trajectory_ds, args.out + '_trajectory', args.format)
     if args.embedding:
+        from matplotlib import pyplot
         nbins = 500
         full_embedding_df = pd.read_csv(args.embedding, sep=None, engine='python', index_col='id')
         xrange = full_embedding_df['x'].min(), full_embedding_df['x'].max()
@@ -56,45 +63,3 @@ def main(argv):
                                                           len(population_list[j].p)))
             figure.savefig(args.out + '_' + str(trajectory_ds.var.index[j]) + '_trajectory.png')
         # plot probabilties on embedding
-
-    unique_days = list(set(trajectory_ds.obs['day']))
-    pair_to_divergenes = {}
-    for i in range(trajectory_ds.shape[1]):
-        for j in range(i):
-            pair_to_divergenes[trajectory_ds.var.index[i], trajectory_ds.var.index[j]] = []
-
-    # create table with time on columns and trajectory pairs on rows
-    for day in unique_days:
-        trajectory_at_day = trajectory_ds[trajectory_ds.obs['day'] == day]
-        for i in range(trajectory_ds.shape[1]):
-            for j in range(i):
-                divergence = 0.5 * np.sum(np.abs(trajectory_at_day[:, i].X - trajectory_at_day[:, j].X))
-                pair_to_divergenes[trajectory_ds.var.index[i], trajectory_ds.var.index[j]].append(divergence)
-
-    if len(populations) > 1:
-        with open(args.out + '_trajectory_divergence.txt', 'w') as f:
-            f.write('pair' + '\t' + 'time' + '\t' + 'divergence' + '\n')
-            for pair in pair_to_divergenes:
-                divergenes = pair_to_divergenes[pair]
-                for i in range(len(divergenes)):
-                    f.write(pair[0] + ' vs. ' + pair[1])
-                    f.write('\t')
-                    f.write(str(unique_days[i]))
-                    f.write('\t')
-                    f.write(str(divergenes[i]))
-                    f.write('\n')
-
-        if args.plot_divergence:
-            divergence_df = pd.read_csv(args.out + '__trajectory_divergence.txt', sep='\t')
-            pyplot.figure(figsize=(10, 10))
-
-            pyplot.xlabel("Day")
-            pyplot.ylabel("Divergence")
-
-            for p, d in divergence_df.groupby('pair'):
-                day = np.asarray(d['time'])
-                divergence = np.asarray(d['divergence'])
-                pyplot.plot(day, divergence, label=p)
-            pyplot.legend()
-            pyplot.savefig(args.out + '_trajectory_divergence.png')
-            # plot all pairs over time
