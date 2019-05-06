07f1e33321545810526e43f8805a05726b036548
jgould <jgould@broadinstitute.org>
2019-05-06 14:43:33 -0400

    Updated example

--- notebooks/command_line_example.sh
+++ notebooks/command_line_example.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # Compute gene set scores, see notebook for an example of converting gene set scores to growth rates
-wot gene_set_scores --matrix data/ExprMatrix.loom \
+wot gene_set_scores \
+--matrix data/ExprMatrix.loom \
 --method mean_z_score \
 --gene_sets data/gene_sets.gmx
 
 # Compute transport maps
-wot optimal_transport --matrix data/ExprMatrix.var.genes.loom \
+wot optimal_transport \
+--matrix data/ExprMatrix.var.genes.loom \
 --cell_days data/cell_days.txt \
 --cell_filter data/serum_cell_ids.txt \
 --growth_iters 3 \
@@ -15,14 +17,16 @@ wot optimal_transport --matrix data/ExprMatrix.var.genes.loom \
 --verbose
 
 # Compute and plot trajectories
-wot trajectory --tmap tmaps/serum \
+wot trajectory \
+--tmap tmaps/serum \
 --cell_set data/major_cell_sets.gmt \
 --day 18 \
 --embedding data/FLE.txt \
 --verbose
 
 # Compute trajectory divergence
-wot trajectory_divergence --trajectory wot_trajectory.txt \
+wot trajectory_divergence \
+--trajectory wot_trajectory.txt \
 --cell_days data/cell_days.txt \
 --matrix data/ExprMatrix.var.genes.loom \
 --compare all \
@@ -30,20 +34,28 @@ wot trajectory_divergence --trajectory wot_trajectory.txt \
 --plot
 
 # Compute and plot trajectory trends
-wot trajectory_trends --trajectory wot_trajectory.txt \
+wot trajectory_trends \
+--trajectory wot_trajectory.txt \
 --cell_days data/cell_days.txt \
 --matrix data/ExprMatrix.loom \
 --gene_filter Nanog,Obox6,Shisa8,Zfp42 \
 --plot
 
 # Compute differentially expressed genes along trajectory
-wot diff_exp --matrix data/ExprMatrix.loom \
+wot diff_exp \
+--matrix data/ExprMatrix.loom \
 --cell_days data/cell_days.txt \
 --trajectory wot_trajectory.txt \
+--nperm 1000 \
 --verbose
 
 # Compute and plot validation summary
-wot optimal_transport_validation --matrix data/ExprMatrix.var.genes.loom \
+wot optimal_transport_validation \
+--matrix data/ExprMatrix.var.genes.loom \
 --cell_days data/cell_days.txt \
---covariate data/batch.txt
+--cell_filter data/serum_cell_ids.txt \
+--covariate data/batches.txt \
+--cell_growth_rates tmaps/serum_g.txt \
+--cell_growth_rates_field g2 \
+--verbose
 
