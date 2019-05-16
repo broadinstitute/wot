427fc52637d64cb2f302be7dfa32fc203410e902
jgould <jgould@broadinstitute.org>
2019-05-16 10:46:31 -0400

    set version

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
-docker build -t wot .
-docker tag wot regevlab/wot
-docker push regevlab/wot
+export WOT_VERSION=1.0.2
+docker build --build-arg WOT_VERSION=$WOT_VERSION -t wot-$WOT_VERSION .
+docker tag wot regevlab/wot-$WOT_VERSION
+docker push regevlab/wot-$WOT_VERSION
