dd116dcd2c72d58cb7e056595d291ebd1be32922
jgould <jgould@broadinstitute.org>
2019-05-29 07:06:15 -0400

    updated population data structure

--- wot/commands/census.py
+++ wot/commands/census.py
@@ -25,11 +25,10 @@ def main(argv):
     cell_sets = wot.io.convert_binary_dataset_to_dict(cell_sets_matrix)
     populations = tmap_model.population_from_cell_sets(cell_sets, at_time=args.day)
 
-    timepoints, census = tmap_model.ancestor_census(cell_sets_matrix, *populations.values())
+    timepoints, census = tmap_model.ancestor_census(cell_sets_matrix, *populations)
 
     obs = pd.DataFrame(index=timepoints)
-    populations_keys = list(populations.keys())
+
     for i in range(len(census)):
-        cs_name = populations_keys[i]
         res = anndata.AnnData(census[i], obs, cell_sets_matrix.var)
-        wot.io.write_dataset(res, args.out + '_' + cs_name, output_format='txt')
+        wot.io.write_dataset(res, args.out + '_' + populations[i].name, output_format='txt')
